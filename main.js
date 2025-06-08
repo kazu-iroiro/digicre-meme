70776cd4ee70b4b2b4b9d70726619109096ecf10
glyzinieh <glyzinie.h@gmail.com>
2025-06-08 16:44:46 +0900

    meme判定を改善

--- main.js
+++ main.js
@@ -18,205 +18,329 @@ function applyCustomDictionary(text, dictionary) {
 
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));//timeはミリ秒
 
-async function memeChecker(text) {
-    const imageBox = document.getElementById("imagebox");
-
-    text = text.replace(/[\s ]/g, "");
-    console.log(text);
-
-    switch (text) {
-        case "怪しい日本語":
-            console.log("怪レい日本語");
-            insertImage("./stamps/ayashiinihongo.png");
-            break;
-
-        case "閉園":
-            console.log("閉園");
-            insertImage("./stamps/heien.png");
-            break;
-
-        case "ほとんど違法":
-            console.log("ほとんど違法");
-            insertImage("./stamps/hotondoihou.webp");
-            break;
-
-        case "偉業":
-        case "いぎょう":
-        case "医療":
-        case "行":
-            console.log("偉業");
-            insertImage("./stamps/igyo.png");
-            break;
-
-        case "いい話":
-        case "良い話":
-            console.log("いい話");
-            insertImage("./stamps/iihanashi.png");
-            break;
-
-        case "しらんけど":
-        case "知らんけど":
-            insertImage("./stamps/shirankedo.jpg");
-            break;
-
-        case "光と闇の行方":
-            insertImage("./stamps/hikaritoyaminoyukue.png");
-            break;
-
-        case "爆誕":
-            insertImage("./stamps/bakutan.png");
-            break;
-
-        case "おふとんに押し込んであげましょうね":
-        case "お布団に押し込んであげましょうね":
-            insertImage("./stamps/ofutonnioshikondeagemasyoune.webp");
-            break;
-
-        case "部室":
-            insertImage("./stamps/bushitu.png");
-            break;
-
-        case "治安が良くない":
-            insertImage("./stamps/tiangayokunai.png");
-            break;
-
-        case "できない":
-            insertImage("./stamps/dekinai.png");
-            break;
-
-        case "できる":
-            insertImage("./stamps/dekiru.png");
-            break;
-
-        case "でたわね":
-        case "出たわね":
-            insertImage("./stamps/detawane.png");
-            break;
-
-        case "英雄の証":
-            insertImage("./stamps/eiyunoakashi.png");
-            break;
-
-        case "ゆるしてくれ":
-        case "許してくれ":
-            insertImage("./stamps/yurushitekure.png");
-            break;
-
-        case "ファッキンホット":
-        case "ハッキングホット":
-        case "ハッキング":
-            insertImage("./stamps/fakinhot.png");
-            break;
-
-        case "深い":
-            insertImage("./stamps/fukai.png");
-            break;
-
-        case "ふわふわ":
-            insertImage("./stamps/fuwafuwa.png");
-            break;
-
-        case "ガハハ":
-            insertImage("./stamps/gahaha.png");
-            break;
-
-        case "エッチなのはいけないと思います":
-            insertImage("./stamps/hnanohaikenaitoomoimasu.png");
-            break;
-
-        case "激しく同意":
-            insertImage("./stamps/hageshikudoui.png");
-            break;
-
-        case "話せばわかる":
-            insertImage("./stamps/hanasebawakaru.png");
-            break;
-
-        case "金がない":
-            insertImage("./stamps/kaneganai.png");
-            break;
-
-        case "危険が危ない":
-            insertImage("./stamps/kikengaabunai.png");
-            break;
-
-        case "これ好き":
-            insertImage("./stamps/koresuki.png");
-            break;
-
-        case "これは":
-            insertImage("./stamps/koreha.png");
-            break;
-
-
-        // 公開版は画像なし
-        case "丸亀製麺":
-            insertImage("./stamps/marugameseimen.png");
-            break;
-
-        case "名言":
-            insertImage("./stamps/meigen.png");
-            break;
-
-        case "寝てくれ":
-            insertImage("./stamps/netekure.png");
-            break;
-
-        case "にこにこ":
-        case "ニコニコ":
-            insertImage("./stamps/nikoniko.png");
-            break;
-
-        case "って思うじゃん":
-        case "で思うじゃん":
-            insertImage("./stamps/teomoujyan.png");
-            break;
-
-        case "俺も俺も":
-            insertImage("./stamps/oremooremo.png");
-            break;
-
-        case "推し":
-            insertImage("./stamps/oshi.png");
-            break;
-
-        case "お疲れ様です":
-            insertImage("./stamps/otsukaresamadesu.png");
-            break;
-
-        case "パワー":
-            insertImage("./stamps/power.png");
-            break;
-
-        case "よし":
-            insertImage("./stamps/yoshi.png");
-            break;
-
-        case "優秀な後輩":
-            insertImage("./stamps/yusyunakouhai.png");
-            break;
-
-        case "そういう日もある":
-            insertImage("./stamps/souiuhimoaru.png");
-            break;
-
-        case "一緒にやろ":
-        case "一緒にやろう":
-            insertImage("./stamps/isyoniyaro.gif");
-            break;
-
-        case "ですわ":
-            insertImage("./stamps/desuwa.jpg");
-            break;
-
-        case "最強":
-            insertImage("./stamps/saikyo.gif");
-            break;
-
-        default:
-            await sleep(5000); // 誤認識で一時的にマッチしないのを防ぐ
-            // clearImage();
+const wordList = [
+    {
+        "word": "怪レい日本語",
+        "targets": [
+            "怪しい日本語"
+        ],
+        "file": "./stamps/ayashiinihongo.png"
+    },
+    {
+        "word": "閉園",
+        "targets": [
+            "閉園"
+        ],
+        "file": "./stamps/heien.png"
+    },
+    {
+        "word": "ほとんど違法",
+        "targets": [
+            "ほとんど違法"
+        ],
+        "file": "./stamps/hotondoihou.webp"
+    },
+    {
+        "word": "偉業",
+        "targets": [
+            "偉業",
+            "いぎょう",
+            "医療",
+            "行"
+        ],
+        "file": "./stamps/igyo.png"
+    },
+    {
+        "word": "いい話",
+        "targets": [
+            "いい話",
+            "良い話"
+        ],
+        "file": "./stamps/iihanashi.png"
+    },
+    {
+        "word": "しらんけど",
+        "targets": [
+            "しらんけど",
+            "知らんけど"
+        ],
+        "file": "./stamps/shirankedo.jpg"
+    },
+    {
+        "word": "光と闇の行方",
+        "targets": [
+            "光と闇の行方"
+        ],
+        "file": "./stamps/hikaritoyaminoyukue.png"
+    },
+    {
+        "word": "爆誕",
+        "targets": [
+            "爆誕"
+        ],
+        "file": "./stamps/bakutan.png"
+    },
+    {
+        "word": "おふとんに押し込んであげましょうね",
+        "targets": [
+            "おふとんに押し込んであげましょうね",
+            "お布団に押し込んであげましょうね"
+        ],
+        "file": "./stamps/ofutonnioshikondeagemasyoune.webp"
+    },
+    {
+        "word": "部室",
+        "targets": [
+            "部室"
+        ],
+        "file": "./stamps/bushitu.png"
+    },
+    {
+        "word": "治安が良くない",
+        "targets": [
+            "治安が良くない"
+        ],
+        "file": "./stamps/tiangayokunai.png"
+    },
+    {
+        "word": "できない",
+        "targets": [
+            "できない"
+        ],
+        "file": "./stamps/dekinai.png"
+    },
+    {
+        "word": "できる",
+        "targets": [
+            "できる"
+        ],
+        "file": "./stamps/dekiru.png"
+    },
+    {
+        "word": "でたわね",
+        "targets": [
+            "でたわね",
+            "出たわね"
+        ],
+        "file": "./stamps/detawane.png"
+    },
+    {
+        "word": "英雄の証",
+        "targets": [
+            "英雄の証"
+        ],
+        "file": "./stamps/eiyunoakashi.png"
+    },
+    {
+        "word": "ゆるしてくれ",
+        "targets": [
+            "ゆるしてくれ",
+            "許してくれ"
+        ],
+        "file": "./stamps/yurushitekure.png"
+    },
+    {
+        "word": "ファッキンホット",
+        "targets": [
+            "ファッキンホット",
+            "ハッキングホット",
+            "ハッキング"
+        ],
+        "file": "./stamps/fakinhot.png"
+    },
+    {
+        "word": "深い",
+        "targets": [
+            "深い"
+        ],
+        "file": "./stamps/fukai.png"
+    },
+    {
+        "word": "ふわふわ",
+        "targets": [
+            "ふわふわ"
+        ],
+        "file": "./stamps/fuwafuwa.png"
+    },
+    {
+        "word": "ガハハ",
+        "targets": [
+            "ガハハ"
+        ],
+        "file": "./stamps/gahaha.png"
+    },
+    {
+        "word": "エッチなのはいけないと思います",
+        "targets": [
+            "エッチなのはいけないと思います"
+        ],
+        "file": "./stamps/hnanohaikenaitoomoimasu.png"
+    },
+    {
+        "word": "激しく同意",
+        "targets": [
+            "激しく同意"
+        ],
+        "file": "./stamps/hageshikudoui.png"
+    },
+    {
+        "word": "話せばわかる",
+        "targets": [
+            "話せばわかる"
+        ],
+        "file": "./stamps/hanasebawakaru.png"
+    },
+    {
+        "word": "金がない",
+        "targets": [
+            "金がない"
+        ],
+        "file": "./stamps/kaneganai.png"
+    },
+    {
+        "word": "危険が危ない",
+        "targets": [
+            "危険が危ない"
+        ],
+        "file": "./stamps/kikengaabunai.png"
+    },
+    {
+        "word": "これ好き",
+        "targets": [
+            "これ好き"
+        ],
+        "file": "./stamps/koresuki.png"
+    },
+    {
+        "word": "これは",
+        "targets": [
+            "これは"
+        ],
+        "file": "./stamps/koreha.png"
+    },
+    {
+        "word": "名言",
+        "targets": [
+            "名言"
+        ],
+        "file": "./stamps/meigen.png"
+    },
+    {
+        "word": "寝てくれ",
+        "targets": [
+            "寝てくれ"
+        ],
+        "file": "./stamps/netekure.png"
+    },
+    {
+        "word": "にこにこ",
+        "targets": [
+            "にこにこ",
+            "ニコニコ"
+        ],
+        "file": "./stamps/nikoniko.png"
+    },
+    {
+        "word": "って思うじゃん",
+        "targets": [
+            "って思うじゃん",
+            "で思うじゃん"
+        ],
+        "file": "./stamps/teomoujyan.png"
+    },
+    {
+        "word": "俺も俺も",
+        "targets": [
+            "俺も俺も"
+        ],
+        "file": "./stamps/oremooremo.png"
+    },
+    {
+        "word": "推し",
+        "targets": [
+            "推し"
+        ],
+        "file": "./stamps/oshi.png"
+    },
+    {
+        "word": "お疲れ様です",
+        "targets": [
+            "お疲れ様です"
+        ],
+        "file": "./stamps/otsukaresamadesu.png"
+    },
+    {
+        "word": "パワー",
+        "targets": [
+            "パワー"
+        ],
+        "file": "./stamps/power.png"
+    },
+    {
+        "word": "よし",
+        "targets": [
+            "よし"
+        ],
+        "file": "./stamps/yoshi.png"
+    },
+    {
+        "word": "優秀な後輩",
+        "targets": [
+            "優秀な後輩"
+        ],
+        "file": "./stamps/yusyunakouhai.png"
+    },
+    {
+        "word": "そういう日もある",
+        "targets": [
+            "そういう日もある"
+        ],
+        "file": "./stamps/souiuhimoaru.png"
+    },
+    {
+        "word": "一緒にやろ",
+        "targets": [
+            "一緒にやろ",
+            "一緒にやろう"
+        ],
+        "file": "./stamps/isyoniyaro.gif"
+    },
+    {
+        "word": "ですわ",
+        "targets": [
+            "ですわ"
+        ],
+        "file": "./stamps/desuwa.jpg"
+    },
+    {
+        "word": "最強",
+        "targets": [
+            "最強"
+        ],
+        "file": "./stamps/saikyo.gif"
     }
+]
+
+async function memeChecker(text) {
+    text = text.replace(/[\s ]/g, "");
+    console.log(`チェック: ${text}`);
+
+    for (const item of wordList) {
+        if (item.targets.includes(text)) {
+            console.log(`マッチ: ${item.word}`);
+            insertImage(item.file);
+
+            if (isConnected) {
+                try {
+                    await sendComment(item.word);
+                } catch (error) {
+                    console.error("コメント送信エラー:", error);
+                }
+                return; // 最初にマッチしたものだけを表示
+            }
+        }
+    }
+    await sleep(5000);
 }
 
 function insertImage(src) {
